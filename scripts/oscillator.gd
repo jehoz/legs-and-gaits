2b9648e777fa1fffe700b7851e0a0b36d8f7b92f
holzmanj <jesseholzman96@gmail.com>
2024-02-29 09:09:13 -0500

    Add asymmetric oscillator and phase offset params

--- scripts/oscillator.gd
+++ scripts/oscillator.gd
@@ -7,14 +7,21 @@ var elapsed_seconds : float = 0.0
 	
 func _process(delta):
 	elapsed_seconds += delta
-	
+
 # SAMPLING FUNCTIONS
-func sine():
-	return sin(frequency * elapsed_seconds + phase)
-	
-func skewed(skew: float):
+func sine(phase_offset: float = 0):
+	return sin(frequency * elapsed_seconds + phase + phase_offset)
+
+func skewed(skew: float, phase_offset: float = 0):
 	if skew == 0:
 		return self.sine()
-	var x = frequency * elapsed_seconds + phase
+	var x = frequency * elapsed_seconds + phase + phase_offset
 	skew = clamp(skew, -1.0, 1.0)
 	return (1.0 / skew) * atan2(skew * sin(x), 1.0 - skew * cos(x))
+
+func asymmetric(bias: float, phase_offset: float = 0):
+	if bias == 0:
+		return self.sine()
+	var x =  frequency * elapsed_seconds + phase + phase_offset
+	var k = pow(2, -bias)
+	return (2.0 * (pow(k, sin(x) + 1.0) - 1.0)) / (pow(k, 2) - 1.0) - 1.0
