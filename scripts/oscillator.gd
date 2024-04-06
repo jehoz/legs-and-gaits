7b21ad6d9822ce4f1fad4cef34d0cc6eeb327a34
holzmanj <jesseholzman96@gmail.com>
2024-04-05 22:16:39 -0400

    Change oscillator frequency calculation for smoother transition

--- scripts/oscillator.gd
+++ scripts/oscillator.gd
@@ -10,22 +10,22 @@ func _init(freq: float = 1.0, phase_offset: float = 0):
 	self.phase = phase_offset
 
 func _process(delta):
-	elapsed_seconds += delta
+	elapsed_seconds += delta * frequency
 
 # SAMPLING FUNCTIONS
 func sine(phase_offset: float = 0):
-	return sin(frequency * elapsed_seconds + phase + phase_offset)
+	return sin(elapsed_seconds + phase + phase_offset)
 
 func skewed(skew: float, phase_offset: float = 0):
 	if skew == 0:
 		return self.sine()
-	var x = frequency * elapsed_seconds + phase + phase_offset
+	var x = elapsed_seconds + phase + phase_offset
 	skew = clamp(skew, -1.0, 1.0)
 	return (1.0 / skew) * atan2(skew * sin(x), 1.0 - skew * cos(x))
 
 func asymmetric(bias: float, phase_offset: float = 0):
 	if bias == 0:
 		return self.sine()
-	var x =  frequency * elapsed_seconds + phase + phase_offset
+	var x =  elapsed_seconds + phase + phase_offset
 	var k = pow(2, -bias)
 	return (2.0 * (pow(k, sin(x) + 1.0) - 1.0)) / (pow(k, 2) - 1.0) - 1.0
